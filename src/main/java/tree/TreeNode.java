9fd675bff7cb9d55f37abc0b94543af7f3735d6e
howey <597740223@qq.com>
2022-02-23 17:05:43 +0800

    arith binary search practise

---
+++ src/main/java/tree/TreeNode.java
@@ -0,0 +1,17 @@
+package tree;
+
+/**
+ * @Description
+ * @Author huanghao
+ * @Date 2022-2-21
+ * @Version 1.0
+ */
+public class TreeNode {
+    TreeNode leftChild;
+    TreeNode rightChild;
+    int data;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+}
