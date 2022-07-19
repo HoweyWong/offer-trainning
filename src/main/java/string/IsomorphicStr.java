8eee28bb2186cc6ae093ce7d1d5e0c35c9d0e6b9
黄浩 <15818421682@163.com>
2022-07-19 16:23:32 +0800

    同构字符串，利用哈希表

--- src/main/java/string/IsomorphicStr.java
+++ src/main/java/string/IsomorphicStr.java
@@ -1,5 +1,8 @@
 package string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 同构字符串
  *
@@ -13,6 +16,24 @@ public class IsomorphicStr {
      * s = "egg", t = "add" true,类似成语的abb格式，aabb，aabc
      */
     static boolean isIsomorphic(String s, String t) {
-        return false;
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Map<Character, Character> tmp = new HashMap<>(20);
+        Map<Character, Character> tmp2 = new HashMap<>(20);
+        for (int i = 0; i < sChars.length; i++) {
+            if ((tmp.containsKey(sChars[i]) && tmp.get(sChars[i]) != tChars[i]) || (tmp2.containsKey(tChars[i]) && tmp2.get(tChars[i]) != sChars[i])) {
+                return false;
+            }
+            tmp.put(sChars[i], tChars[i]);
+            tmp2.put(tChars[i], sChars[i]);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isIsomorphic("add", "egg"));
+        System.out.println(isIsomorphic("foo", "bar"));
+        System.out.println(isIsomorphic("paper", "title"));
+        System.out.println(isIsomorphic("badc", "baba"));
     }
 }
