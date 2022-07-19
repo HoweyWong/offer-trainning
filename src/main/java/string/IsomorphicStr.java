804f9bf1062d4c599fe901a09fb04532913540bc
黄浩 <15818421682@163.com>
2022-07-19 15:18:08 +0800

    同构字符串

---
+++ src/main/java/string/IsomorphicStr.java
@@ -0,0 +1,18 @@
+package string;
+
+/**
+ * 同构字符串
+ *
+ * @author huanghao
+ * @version 1.0
+ * @date 2022/7/19 15:14
+ */
+public class IsomorphicStr {
+    /**
+     * 如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。
+     * s = "egg", t = "add" true,类似成语的abb格式，aabb，aabc
+     */
+    static boolean isIsomorphic(String s, String t) {
+        return false;
+    }
+}
