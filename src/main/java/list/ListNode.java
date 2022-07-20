15586c1b3bf69cf6342b99c51f2df8eb7168f157
黄浩 <15818421682@163.com>
2022-07-20 16:38:32 +0800

    合并两个有序链表

---
+++ src/main/java/list/ListNode.java
@@ -0,0 +1,33 @@
+package list;
+
+/**
+ * 链表节点
+ *
+ * @author huanghao
+ * @version 1.0
+ * @date 2022/7/20 16:37
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
