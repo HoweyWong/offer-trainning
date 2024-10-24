50c6bd9d00940558ae42c0e7b8828daccb25a3fb
huanghao <15818421682@163.com>
2024-10-24 18:09:11 +0800

    补充算法说明

--- src/main/java/array/IntersectionSizeTwo.java
+++ src/main/java/array/IntersectionSizeTwo.java
@@ -1,6 +1,8 @@
 package array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.OptionalInt;
 
 /**
@@ -42,5 +44,37 @@ public class IntersectionSizeTwo {
                 {1, 2}, {2, 3}, {2, 4}, {4, 5}
         };
         System.out.println(intersectionSizeTwo(b));
+    }
+
+    public int intersectionSizeTwoPass(int[][] intervals) {
+        int n = intervals.length;
+        int res = 0;
+        int m = 2;
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            }
+            return a[0] - b[0];
+        });
+        List<Integer>[] temp = new List[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = new ArrayList<Integer>();
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = intervals[i][0], k = temp[i].size(); k < m; j++, k++) {
+                res++;
+                help(intervals, temp, i - 1, j);
+            }
+        }
+        return res;
+    }
+
+    public void help(int[][] intervals, List<Integer>[] temp, int pos, int num) {
+        for (int i = pos; i >= 0; i--) {
+            if (intervals[i][1] < num) {
+                break;
+            }
+            temp[i].add(num);
+        }
     }
 }
